bff211c97aba29b6c9cbf850db2199080a967346
htshinichi <33441642+htshinichi@users.noreply.github.com>
2018-12-25 13:20:26 +0800

    Create ModelPath.py

---
+++ OnnxTool/OnnxModel/ModelPath.py
@@ -0,0 +1,10 @@
+ROOT_PATH = "/home/hanamaru/project/OnnxTool/"
+####onnx
+ONNX_PATH = ROOT_PATH +"OnnxModel/"
+####caffe
+CAFFE_PATH = ONNX_PATH +"FromCaffe/"
+####cntk
+CNTK_PATH = ONNX_PATH +"FromCntk/"
+####torch
+TORCH_PATH = ONNX_PATH +"FromTorch/"
+
